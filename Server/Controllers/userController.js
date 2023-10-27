b890af274edb1035096b95439d81563c066eaf14
kota-karthik <kotakarthik2307@gmail.com>
2023-10-27 22:42:22 +0530

    corrected a bug in backend

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -1,30 +1,30 @@
 const fs = require("fs");
-const User= require("../Models/userModel");
+const User = require("../Models/userModel");
 const bcrypt = require('bcryptjs');
 
 const express = require("express");
 exports.getAllUsers = async (req, res) => {
- try{
-  const users = await User.find();
-  res.status(200).json({
-    status: "success",
-    results: users.length,
-    data: {
-      users,
-    },
-  });
- }catch(err){
-  console.log("unable to create user");
-  res.status(400).json({
-    status: "error",
-    message: err,
-  });
- }
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (err) {
+    console.log("unable to create user");
+    res.status(400).json({
+      status: "error",
+      message: err,
+    });
+  }
 };
 
 exports.createUser = async (req, res) => {
 
-  try{
+  try {
     const newUser = await User.create(req.body);
     res.status(201).json({
       status: "success",
@@ -32,8 +32,8 @@ exports.createUser = async (req, res) => {
         user: newUser
       },
     });
-  }catch(err){
-    
+  } catch (err) {
+
     console.log("unable to create user");
     res.status(400).json({
       status: "error",
@@ -43,63 +43,62 @@ exports.createUser = async (req, res) => {
 };
 
 exports.updateUser = async (req, res) => {
-try{
-  // const password=await bcrypt.hash(req.body.password, 12);
-  // const {name,password,email}=req.body;
-  // const newPassword =await bcrypt.hash(password, 12);
-  // console.log(newPassword)
-const user= await User.findByIdAndUpdate(req.params.id, req.body, {
-  new: true,
-  runValidators: true,
-});
-console.log(user);
-res.status(200).json({
-  status: "success",
-  data:{
-    user,
-  }
-})
-}catch(err){
-  console.log("unable to update password");
+  try {
+   const newPassword=await bcrypt.hash(req.body.password, 12);
+    const user = await User.findByIdAndUpdate(req.params.id,{"password":newPassword} , {
+      new: true,
+      runValidators: true,
+
+    });
+    console.log(user);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      }
+    })
+  } catch (err) {
+    console.log("unable to update password");
     res.status(400).json({
       status: "error",
       message: err,
     });
-}
+
+  }
 };
 
 exports.getUniqueUser = async (req, res) => {
- try{
-const user =await User.findById(req.params.id);
-res.status(200).json({
-  status: "success",
-  data:{
-    user,
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      }
+    })
+  } catch (err) {
+    console.log("User not found");
+    res.status(400).json({
+      status: "error",
+      message: err,
+    });
   }
-})
- }catch(err){
-  console.log("User not found");
-  res.status(400).json({
-    status: "error",
-    message: err,
-  });
- }
 
 };
 exports.deleteUser = async (req, res) => {
-try{
-await User.findByIdAndDelete(req.params.id);
-res.status(200).json({
-  status: "success",
-  data: null,
-});
-}catch(err){
-  console.log("unable to delete user");
-  res.status(200).json({
-    status: "success",
-    message: err,
-  });
-}
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    console.log("unable to delete user");
+    res.status(200).json({
+      status: "success",
+      message: err,
+    });
+  }
 };
 
 
--- Server/Models/userModel.js
+++ Server/Models/userModel.js
@@ -35,7 +35,7 @@ userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = undefined;
+  this.passwordConfirm = await bcrypt.hash(this.passwordConfirm, 12);
   next();
 });
 
